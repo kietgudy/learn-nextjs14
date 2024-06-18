950a56f1b9e29b0e8e36ae2440b81edd6a3a68a7
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-06-18 14:45:00 +0700

    set up project

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import ThemeRegistry from "@/components/theme-registry/theme.registry";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeRegistry>{children}</ThemeRegistry>
+      </body>
+    </html>
+  );
+}
