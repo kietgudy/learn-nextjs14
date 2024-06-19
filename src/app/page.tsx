e219d88d0ead1cae78772edff0f4b9cc46811bde
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-06-19 14:28:33 +0700

    fetch data category sound

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,27 @@ import { Container } from "@mui/material";
 import { sendRequest } from "@/utils/api";
 
 export default async function HomePage() {
-  const res = await sendRequest<IBackendRes<ITrackTop[]>>({
+  const chills = await sendRequest<IBackendRes<ITrackTop[]>>({
     url: "http://localhost:8000/api/v1/tracks/top",
     method: "POST",
     body: { category: "CHILL", limit: 10 },
   });
+  const workouts = await sendRequest<IBackendRes<ITrackTop[]>>({
+    url: "http://localhost:8000/api/v1/tracks/top",
+    method: "POST",
+    body: { category: "WORKOUT", limit: 10 },
+  });
+  const party = await sendRequest<IBackendRes<ITrackTop[]>>({
+    url: "http://localhost:8000/api/v1/tracks/top",
+    method: "POST",
+    body: { category: "PARTY", limit: 10 },
+  });
   return (
     <>
       <Container>
-        <MainSlider />
-        <MainSlider />
-        <MainSlider />
+        <MainSlider data={chills.data ?? []}/>
+        <MainSlider data={workouts.data ?? []}/>
+        <MainSlider data={party.data ?? []}/>
       </Container>
     </>
   );
--- src/components/main/MainSlider.tsx
+++ src/components/main/MainSlider.tsx
@@ -1,109 +1,113 @@
-'use client'
+"use client";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { Settings } from "react-slick";
 import { Box } from "@mui/material";
 import Button from "@mui/material/Button/Button";
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import Divider from '@mui/material/Divider';
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import Divider from "@mui/material/Divider";
 
-const MainSlider = () => {
-    const NextArrow = (props: any) => {
-        return (
-            <Button variant="outlined"
-                onClick={props.onClick}
-                sx={{
-                    position: "absolute",
-                    right: 0,
-                    top: "50%",
-                    zIndex: 2,
-                    minWidth: 30,
-                    width: 35,
-                }}
-            >
-                <ChevronRightIcon />
-            </Button>
-        )
-    }
-
-    const PrevArrow = (props: any) => {
-        return (
-            <Button variant="outlined" onClick={props.onClick}
-                sx={{
-                    position: "absolute",
-                    top: "50%",
-                    zIndex: 2,
-                    minWidth: 30,
-                    width: 35,
-                }}
-            >
-                <ChevronLeftIcon />
-            </Button>
-        )
-    }
-
-    const settings: Settings = {
-        infinite: true,
-        speed: 500,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-    };
-    //box === div
-    return (
-
-        <Box
-            sx={{
-                margin: "0 50px",
-                ".item": {
-                    padding: "0 10px"
-                },
-                "h3": {
-                    border: "1px solid #ccc",
-                    padding: "20px",
-                    height: "200px",
-
-                }
-            }}
-        >
-            <h2> Multiple tracks </h2>
-
-            <Slider {...settings}>
-                <div className="item">
-                    <h3>Track 1</h3>
-                </div>
-                <div className="item">
-                    <h3>Track 2</h3>
-                </div>
-                <div className="item">
-                    <h3>Track 3</h3>
-                </div>
-                <div className="item">
-                    <h3>Track 4</h3>
-                </div>
-                <div className="item">
-                    <h3>Track 5</h3>
-                </div>
-                <div className="item">
-                    <h3>Track 6</h3>
-                </div>
-                <div className="item">
-                    <h3>Track 7</h3>
-                </div>
-                <div className="item">
-                    <h3>Track 8</h3>
-                </div>
-                <div className="item">
-                    <h3>Track 9</h3>
-                </div>
-            </Slider>
-            <Divider />
-        </Box>
-
-    );
+interface IProps {
+  data: ITrackTop[];
 }
+
+const MainSlider = (props: IProps) => {
+  const NextArrow = (props: any) => {
+    return (
+      <Button
+        variant="outlined"
+        onClick={props.onClick}
+        sx={{
+          position: "absolute",
+          right: 0,
+          top: "50%",
+          zIndex: 2,
+          minWidth: 30,
+          width: 35,
+        }}
+      >
+        <ChevronRightIcon />
+      </Button>
+    );
+  };
+
+  const PrevArrow = (props: any) => {
+    return (
+      <Button
+        variant="outlined"
+        onClick={props.onClick}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          zIndex: 2,
+          minWidth: 30,
+          width: 35,
+        }}
+      >
+        <ChevronLeftIcon />
+      </Button>
+    );
+  };
+
+  const settings: Settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+  //box === div
+  return (
+    <Box
+      sx={{
+        margin: "0 50px",
+        ".item": {
+          padding: "0 10px",
+        },
+        h3: {
+          border: "1px solid #ccc",
+          padding: "20px",
+          height: "200px",
+        },
+      }}
+    >
+      <h2> Multiple tracks </h2>
+
+      <Slider {...settings}>
+        <div className="item">
+          <h3>Track 1</h3>
+        </div>
+        <div className="item">
+          <h3>Track 2</h3>
+        </div>
+        <div className="item">
+          <h3>Track 3</h3>
+        </div>
+        <div className="item">
+          <h3>Track 4</h3>
+        </div>
+        <div className="item">
+          <h3>Track 5</h3>
+        </div>
+        <div className="item">
+          <h3>Track 6</h3>
+        </div>
+        <div className="item">
+          <h3>Track 7</h3>
+        </div>
+        <div className="item">
+          <h3>Track 8</h3>
+        </div>
+        <div className="item">
+          <h3>Track 9</h3>
+        </div>
+      </Slider>
+      <Divider />
+    </Box>
+  );
+};
 
 export default MainSlider;
